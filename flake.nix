d369ff703c9a676ab9b73726576bd5f59e9d8fd6
Kevin_Chen <108531516+Kevin-2483@users.noreply.github.com>
2024-08-26 09:41:04 +0800

    add Darwin modules

--- flake.nix
+++ flake.nix
@@ -3,6 +3,6 @@
 
   outputs = _: {
     homeManagerModules.catppuccin = import ./modules/home-manager;
-    # nixosModules.catppuccin = import ./modules/nixos;
+    darwinModules.catppuccin = import ./modules/nixdarwin;
   };
 }
